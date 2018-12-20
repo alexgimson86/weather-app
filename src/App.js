b24fd1a0472cbbc8d93d858ce139dd02560aac74
alexgimson86 <alexgimson@gmail.com>
2018-12-20 14:28:47 -0600

    fixed an error in app.js file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Form from './components/Form.js'
 import Weather from './components/Weather.js'
 const Api_Key = '5f44a3c33e11771fa7b43cee2023732c'
 class App extends Component {
-  getWeather = async() => {
+  getWeather = async(e) => {
+    e.preventDefault();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${Api_Key}`);
   
     const response = await api_call.json();
@@ -17,7 +18,7 @@ class App extends Component {
     return (
       <div className="App">
         <Titles />
-        <Form loadWeather={this.getWeather()}/>
+        <Form loadWeather={this.getWeather}/>
 
       </div>
     );
