4230bfaa00fd14769a94dac20736e8555f2af149
alexgimson86 <alexgimson@gmail.com>
2019-01-02 18:55:33 -0600

    added five day forecast

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import Titles from './components/Title.js'
 import Form from './components/Form.js'
 import Weather from './components/Weather.js'
 const Api_Key = '5f44a3c33e11771fa7b43cee2023732c'
-const subT = 273.15
-const nF = 9.0/5.0
-const addTh = 32
+
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
       city:null,
       country:null,
-      temperature: null,//response.main.temp,
-      humidity: null,//response.main.humidity,
-      error:null,
-      description: null,//response.weather[0].description,
-      icon: null
+     // temperature: [],//response.main.temp,
+      weather: [],//response.main.humidity,
     }
   }
   getWeather = async(e) => {
@@ -27,17 +22,14 @@ class App extends Component {
     let co = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${c},${co}&APPID=${Api_Key}`);
     const response = await api_call.json();
-    let farenheight = (response.list[0].main.temp - subT)*nF + addTh
-    console.log(response.list[0].main.humidity)
-    console.log(response.list[0].weather[0].description)
+   // let farenheight = (response.list[0].main.temp - subT)*nF + addTh
+    //console.log(response.list[0].main.humidity)
+    //console.log(response.list[0].weather[0].description)
         this.setState({
           city: c,
           country: co,
-          temperature: farenheight,
-          humidity: response.list[0].main.humidity,
-          error: "",
-          description: response.list[0].weather[0].description,
-          icon: response.list[0].weather[0].icon
+         // temperature: farenheight,
+          weather: response.list,
         })
   }
   render() {
@@ -51,9 +43,7 @@ class App extends Component {
           <Weather city={this.state.city} 
                   country={this.state.country}
                   temperature={this.state.temperature}
-                  humidity={this.state.humidity}
-                  description={this.state.description} 
-                  icon={this.state.icon}
+                  weather={this.state.weather}
                   />
        </div>
       </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,62 +1,69 @@
 import React from 'react';
 const Weather = (props) =>{
-
-  const iconLink = 'http://openweathermap.org/img/w/' + props.icon + '.png'
+  const subT = 273.15
+  const nF = 9.0/5.0
+  const addTh = 32
+  const iconLink = 'http://openweathermap.org/img/w/';
   const date = new Date();
   let day = date.getDay();
-  let dayRay = ["Monday", "Tuesday","Wednesday","Thursday", "Friday", "Saturday" , "Sunday"]
-  
+  let dayRay = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday", "Friday", "Saturday"]
+  let titles = ["Day","City", "Country","Temperature","Humidity","Description"]
+  let tableData = [];
+  let formattedData = [[dayRay[day]],[dayRay[(++day)%dayRay.length]],[dayRay[(++day)%dayRay.length]],[dayRay[(++day)%dayRay.length]],[dayRay[(++day)%dayRay.length]]];
+  let iconArr = [];
+  let farenheight;
+  if(props.weather.length !== 0){
+    tableData = [props.weather[4],props.weather[12], props.weather[20], props.weather[28], props.weather[36]];
+    tableData.forEach((day, i )=>{
+      formattedData[i].push(props.city)
+      formattedData[i].push(props.country)
+      farenheight = (day.main.temp_max - subT)*nF + addTh;
+      formattedData[i].push(Math.round(farenheight) + " Farenheight")
+      formattedData[i].push(day.main.humidity + "%")
+      formattedData[i].push(day.weather[0].description)
+      const fullIcon = iconLink + day.weather[0].icon + ".png"
+      iconArr.push(fullIcon)
+   })
+    /*for(let i = 0; i < props.weather.length; i += 7){
+        tableData.push(`<td>${JSON.stringify(props.weather[i].dt)}</td><td>${dayRay[day]}</td><td>${JSON.stringify(props.city)}</td><td>${JSON.stringify(props.country)}</td><td>${JSON.stringify(props.weather[i].main.humidity)}</td><td>${JSON.stringify(props.weather[i].weather[0].description)}</td><td>${iconLink + JSON.stringify(props.weather[i].weather[0].icon)}</td>`);
+    }*/
+  }
   let forecastFirst = (
     <div>
       <table className="table">
         <thead className="thead-light">
         <tr>
-          <th scope="col">
-            Day
-          </th>
-          <th scope="col">
-            City
-          </th>
-          <th scope="col">
-            Country
-          </th>
-          <th scope="col">
-            Temp
-          </th>
-          <th scope="col">
-            Humidity
-          </th>
-          <th scope="col">
-            Description
-          </th>
-          <th scope="col">
-          </th>
+          {titles.map(title =>
+            <th scope="col" key={title}>{title}</th>
+          )}
         </tr>
           </thead>
         <tbody>
-        <tr>
-          <th scope="row">
-            {dayRay[day -1]}
-          </th>
-          <td>
-            {props.city}
-          </td>
-          <td>
-            {props.country}
-          </td>
-          <td>
-            {Math.floor(props.temperature)} deg/F
-          </td>
-          <td>
-            {props.humidity}
-          </td>
-          <td>
-            {props.description}
-          </td>
-          <td>
-           <img src={iconLink} alt="" />
-          </td>
-        </tr>
+          <tr>
+           {formattedData[0].map( (data, i) => 
+              <td key={i}>{data}</td>)}
+              <td key={iconArr[0]}><img src={iconArr[0]} alt=""></img></td>
+          </tr>
+          <tr>
+           {formattedData[1].map( (data, i) => 
+              <td key={i}>{data}</td>)}
+              <td key={iconArr[1]}><img src={iconArr[1]} alt=""></img></td>
+          </tr>
+          <tr>
+           {formattedData[2].map( (data, i) => 
+              <td key={i}>{data}</td>)}
+              <td key={iconArr[2]}><img src={iconArr[2]} alt=""></img></td>
+          </tr>
+          <tr>
+           {formattedData[3].map( (data, i) => 
+              <td key={i}>{data}</td>)}
+              <td key={iconArr[3]}><img src={iconArr[3]} alt=""></img></td>
+          </tr>
+          <tr>
+           {formattedData[4].map( (data, i) => 
+              <td key={i}>{data}</td>)}
+              <td key={iconArr[4]}><img src={iconArr[4]} alt=""></img></td>
+          </tr>
         </tbody>
       </table>
     </div>
